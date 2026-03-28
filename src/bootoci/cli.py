e39e54f8b541d07201539d3276eb2607346ed7c4
Nadav Tasher <tashernadav@gmail.com>
2026-03-28 20:20:25 +0300

    General improvements, added --debug, --login, images are now RAW with an actual ext4 partition

--- src/bootoci/cli.py
+++ src/bootoci/cli.py
@@ -1,3 +1,4 @@
+import uuid
 import argparse
 
 from .oci_to_bootable import oci_to_bootable
@@ -8,31 +9,46 @@ def parse_arguments():
     parser = argparse.ArgumentParser()
 
     # Output directory
-    parser.add_argument("-o", "--output", action="store", default="bin", help="Output directory")
-
-    # Use serial for kernel boot?
-    parser.add_argument("--serial", action="store_true", help="Use serial for kernel boot")
+    parser.add_argument("--output", "-o", action="store", default="bin", help="Output directory")
 
     # How are we building this?
-    runtime_group = parser.add_mutually_exclusive_group(required=True)
-    runtime_group.add_argument("--docker", action="store_true", help="Use docker to build image")
-    runtime_group.add_argument("--podman", action="store_true", help="Use podman to build image")
-
-    # Where does the kernel come from?
-    kernel_group = parser.add_mutually_exclusive_group(required=True)
-    kernel_group.add_argument("--kernel-from-host", action="store_true", help="Use host's kernel")
-    kernel_group.add_argument("--kernel-from-debian", action="store_true", help="Use debian's kernel")
-    kernel_group.add_argument("--kernel-from-source", action="store_true", help="Build kernel from upstream source")
+    backend_group = parser.add_mutually_exclusive_group(required=True)
+    backend_group.add_argument("--backend-docker", "--docker", action="store_true", help="Use docker to build image")
+    backend_group.add_argument("--backend-podman", "--podman", action="store_true", help="Use podman to build image")
 
     # Where does the rootfs come from?
     rootfs_group = parser.add_mutually_exclusive_group(required=True)
-    rootfs_group.add_argument("-t", "--tag", action="store", help="Tag to build from")
-    rootfs_group.add_argument("-f", "--dockerfile", action="store", help="Path to Dockerfile")
+    rootfs_group.add_argument("--rootfs-from-tag", "--tag", "-t", action="store", help="Tag to build from")
+    rootfs_group.add_argument("--rootfs-from-dockerfile", "--dockerfile", "-f", action="store", help="Path to Dockerfile")
 
-    # Where does init come from?
-    init_group = parser.add_mutually_exclusive_group(required=True)
-    init_group.add_argument("--ash", action="store_true", help="Boot directly into BusyBox ash, statically compiled")
-    init_group.add_argument("--systemd", action="store_true", help="Boot using systemd")
+    # Where does the kernel come from? Optional. Image can already have kernel.
+    kernel_group = parser.add_mutually_exclusive_group(required=False)
+    kernel_group.add_argument("--kernel-from-debian", action="store_true", help="Use debian's kernel")
+    kernel_group.add_argument("--kernel-from-source", action="store_true", help="Build kernel from upstream source")
+
+    # Where does init come from? Optional. Image can already have init.
+    init_group = parser.add_mutually_exclusive_group(required=False)
+    init_group.add_argument("--init-is-ash", "--ash", action="store_true", help="Boot to shell")
+    init_group.add_argument("--init-is-login", "--login", action="store_true", help="Boot to login")
+    init_group.add_argument("--init-is-systemd", "--systemd", action="store_true", help="Boot to systemd")
+
+    # GUID for rootfs partition
+    parser.add_argument("--image-partuuid", "--partuuid", action="store", default="00000000-0000-0000-0000-000000000000", help="Partition identifier for rootfs")
+
+    # Desired image size (affects rootfs size)
+    parser.add_argument("--image-size", "--size", type=int, action="store", default=1024, help="Full image size in MB")
+
+    # Use debug for kernel boot?
+    parser.add_argument("--boot-debug", "--debug", action="store_true", help="Boot kernel in debug mode")
+
+    # Use serial for kernel boot?
+    parser.add_argument("--boot-serial", "--serial", action="store_true", help="Use serial for kernel boot")
+
+    # Change password in rootfs?
+    parser.add_argument("--rootfs-password", "--password", action="store", help="Password to set in USER:PASSWORD format")
+
+    # Change hostname in rootfs?
+    parser.add_argument("--rootfs-hostname", "--hostname", action="store", help="Hostname for system")
 
     # TODO --secure-boot forces --kernel-from-debian
 
--- src/bootoci/oci_to_bootable.py
+++ src/bootoci/oci_to_bootable.py
@@ -1,33 +1,92 @@
 import os
+import hashlib
 import subprocess
+
+# For typing
+from typing import Optional
 
 # For template rendering
 import jinja2
 
 
-def oci_to_bootable(*, output: str = None, serial: bool = None, docker: bool = None, podman: bool = None, dockerfile: str = None, tag: str = None, kernel_from_host: bool = None, kernel_from_source: bool = None, kernel_from_debian: bool = None, ash: bool = None, systemd: bool = None):
-    # Decide which runtime we are using
-    runtime = ["false"]
+def oci_to_bootable(
+    *,
 
-    if docker:
-        # We are using docker as our runtime
-        runtime = ["docker"]
+    # Output directory
+    output: str,
 
-    if podman:
-        # We are using podman as our runtime
-        raise NotImplementedError()
+    # Full image size
+    image_size: int = 1024,
+
+    # Partition UUID for rootfs
+    image_partuuid: str = "00000000-0000-0000-0000-000000000000",
+
+    # Do we boot in debug?
+    boot_debug: bool = False,
+
+    # Do we boot to serial?
+    boot_serial: bool = False,
+
+    # Do we set a rootfs password?
+    rootfs_password: Optional[str] = None,
+
+    # Do we set a rootfs hostname?
+    rootfs_hostname: Optional[str] = None,
+
+    # Are we using docker as a backend?
+    backend_docker: bool = True,
+
+    # Are we using podman as a backend?
+    backend_podman: bool = False,
+
+    # Dockerfile that builds rootfs
+    rootfs_from_dockerfile: Optional[str] = None,
+
+    # Tag that can be used as rootfs
+    rootfs_from_tag: Optional[str] = None,
+
+    # Are we using a kernel from debian?
+    kernel_from_debian: bool = False,
+
+    # Are we building a kernel from source?
+    kernel_from_source: bool = True,
+
+    # Are we using BusyBox ash as the init program?
+    init_is_ash: bool = True,
+
+    # Are we using BusyBox login as the init program?
+    init_is_login: bool = False,
+
+    # Are we using systemd as the init program?
+    init_is_systemd: bool = False,
+
+    # Internal options
+    internal_debian_image: str = "docker.io/library/debian:13-slim",
+    internal_busybox_image: str = "docker.io/library/busybox:1.37.0-musl",
+):
+    # Decide which backend we are using
+    backend = ["false"]
+
+    if backend_docker:
+        # We are using docker as our backend
+        backend = ["docker"]
+
+    if backend_podman:
+        # We are using podman as our backend
+        raise NotImplementedError("Podman isn't supported yet")
+
+    # SystemD init is not supported yet
+    if init_is_systemd:
+        raise NotImplementedError("SystemD isn't supported yet")
 
     # Let's build the base image
-    if dockerfile:
-        # Generate input tag
-        tag = "rootfs-" + os.urandom(5).hex()
+    if rootfs_from_dockerfile:
+        # Generate tag if required
+        if not rootfs_from_tag:
+            rootfs_from_tag = "rootfs-" + os.urandom(4).hex()
 
         # Build the dockerfile
-        subprocess.run(runtime + ["build", "--file", dockerfile, "--tag", tag, os.path.dirname(dockerfile)], check=True)
-
-    if kernel_from_host:
-        # We'll use the host's kernel
-        raise NotImplementedError()
+        subprocess.run(backend + ["build", "--file", rootfs_from_dockerfile, "--tag", rootfs_from_tag, os.path.dirname(rootfs_from_dockerfile)], check=True)
 
     # Create jinja2 template
     with open(os.path.join(os.path.dirname(__file__), "templates", "Dockerfile"), "r", encoding="utf-8") as dockerfile:
@@ -35,24 +94,39 @@ def oci_to_bootable(*, output: str = None, serial: bool = None, docker: bool = N
 
     # Render the template
     rendered_template = template.render(
-        # Base rootfs tag
-        tag=tag,
+        # At this point we already have a rootfs tag
+        rootfs_from_tag=rootfs_from_tag,
+
+        # Additional rootfs options
+        rootfs_password=rootfs_password,
+        rootfs_hostname=rootfs_hostname,
 
         # Kernel source selection
-        kernel_from_host=kernel_from_host,
         kernel_from_debian=kernel_from_debian,
         kernel_from_source=kernel_from_source,
 
         # Init system selection
-        ash=ash,
-        systemd=systemd,
+        init_is_ash=init_is_ash,
+        init_is_login=init_is_login,
+        init_is_systemd=init_is_systemd,
 
-        # Serial for kernel boot?
-        serial=serial,
+        # Full image size
+        image_size=image_size,
+
+        # Partition PARTUUID
+        image_partuuid=image_partuuid,
+
+        # Boot options
+        boot_debug=boot_debug,
+        boot_serial=boot_serial,
+
+        # Internal options
+        internal_debian_image=internal_debian_image,
+        internal_busybox_image=internal_busybox_image,
     )
 
     # Create the output directory
     os.makedirs(output, exist_ok=True)
 
     # Now let's build the image
-    subprocess.run(runtime + ["build", "--file", "-", "--output", output, "."], input=rendered_template, check=True, text=True)
+    subprocess.run(backend + ["build", "--file", "-", "--target", "output", "--output", output, "."], input=rendered_template, check=True, text=True)
